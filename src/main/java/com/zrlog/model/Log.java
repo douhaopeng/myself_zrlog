b8c8662c3268ef8ba48f52a4b52003794869fd79
Heber <1591994251@qq.com>
2018-02-01 10:54:07 +0800

    first commit

--- src/main/java/com/zrlog/model/Log.java
+++ src/main/java/com/zrlog/model/Log.java
@@ -4,4 +4,6 @@ import com.jfinal.plugin.activerecord.Model;
 
 public class Log extends Model<Log> {
    public static final Log dao =  new Log();
+
+
 }
--- src/main/java/com/zrlog/model/Plugin.java
+++ src/main/java/com/zrlog/model/Plugin.java
@@ -2,6 +2,12 @@ package com.zrlog.model;
 
 import com.jfinal.plugin.activerecord.Model;
 
+import java.util.List;
+
 public class Plugin extends Model<Plugin> {
     public static final Plugin dao = new Plugin();
+
+    public List<Plugin> queryAll() {
+        return find("select * from plugin where level>?", 0);
+    }
 }
