8cbf119d513cd30df9f5829b71bad940fe0396fd
Heber <1591994251@qq.com>
2018-02-05 16:57:09 +0800

    first commit

--- src/main/java/com/zrlog/web/controller/blog/PostController.java
+++ src/main/java/com/zrlog/web/controller/blog/PostController.java
@@ -2,6 +2,11 @@ package com.zrlog.web.controller.blog;
 
 import com.zrlog.web.controller.BaseController;
 
+import java.util.Map;
+
 public class PostController extends BaseController{
-    private
+    private void setPageInfo(String currentUri, Map<String, Object> data, int currentPage){
+        setAttr("yurl",currentUri);
+        Integer total = (Integer) data.get("total");
+    }
 }
